19cabd796576e739412f09f308ec9e10b097a839
Erebonia <52137104+Erebonia@users.noreply.github.com>
2024-04-06 11:04:03 -1000

    Attack Animations
    - Animations created and added
    - Can attack while moving / idle state

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,13 +4,32 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const Friction = 500
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
+
 #Initializes and grabs the animation player and tree
 @onready var animationPlayer = $AnimationPlayer
 @onready var animationTree = $AnimationTree
 @onready var animationState = animationTree.get("parameters/playback")
 
+func _ready():
+	animationTree.active = true
+
 func _physics_process(delta):
-	
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state()
+
+func move_state(delta):
 	#This smooths out movements when player is moving in two directions at once.
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("Move_Right") - Input.get_action_strength("Move_Left")
@@ -23,13 +42,29 @@ func _physics_process(delta):
 		#Switches to the proper animations based on our position with blend trees.
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
-		velocity += input_vector * ACCELERATION * delta
-		velocity = velocity.limit_length(MAX_SPEED)
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		
+		#Lets player attack whilst moving.
+		if Input.is_action_just_pressed("attack"):
+			state = ATTACK
 	else:
 		animationState.travel("Idle")
 		#Slow us down when we are stopping movement.
 		velocity = velocity.move_toward(Vector2.ZERO, Friction * delta)
 		
+		#Lets player attack during idle.
+		if Input.is_action_just_pressed("attack"):
+			state = ATTACK
+		
 	move_and_slide()
 
+func attack_state():
+	#Stops the player from sliding cause they were moving.
+	velocity = Vector2.ZERO
+	
+	animationState.travel("Attack")
+	
+func attack_animation_finished():
+	state = MOVE
