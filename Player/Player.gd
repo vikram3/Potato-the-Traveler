3d5b8e37dd622a457c3786b479ea248f7efd5c39
Erebonia <52137104+Erebonia@users.noreply.github.com>
2024-04-06 14:01:03 -1000

    Hurtboxes, hitbox, project layers
    Hitbox on sword and hurtbox on grass.
    
    Added layers to differentiate player, enemy, world, etc

--- Player/Player.gd
+++ Player/Player.gd
@@ -20,7 +20,7 @@ var state = MOVE
 func _ready():
 	animationTree.active = true
 
-func _process(delta):
+func _physics_process(delta):
 	match state:
 		MOVE:
 			move_state(delta)
--- World/grass.gd
+++ World/grass.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		#Load Scene
-		var GrassEffect = load("res://Effects/grass_effect.tscn")
-		#Instance the Scene
-		var grassEffect = GrassEffect.instantiate()
+func create_grass_effect():
+	#Load Scene
+	var GrassEffect = load("res://Effects/grass_effect.tscn")
+	#Instance the Scene
+	var grassEffect = GrassEffect.instantiate()
+	# Get access to the world, add it as a child and set the position.
+	var world = get_tree().current_scene
+	world.add_child(grassEffect)
+	grassEffect.global_position = global_position
 	
-		# Get access to the world, add it as a child and set the position.
-		var world = get_tree().current_scene
-		world.add_child(grassEffect)
-		grassEffect.global_position = global_position
-		
-		queue_free()
+	queue_free()
+
+func _on_hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
