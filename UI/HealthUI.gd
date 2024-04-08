660dbdbd6e166735f9e9f35d6d7e80b2fe490ddf
Erebonia <52137104+Erebonia@users.noreply.github.com>
2024-04-07 21:58:26 -1000

    HP Bar added to UI
    Aside from the HP Bar on the player I have added one on the canvas. Top left.

--- UI/HealthUI.gd
+++ UI/HealthUI.gd
@@ -8,6 +8,24 @@ var max_hearts = 4:
 @onready var label = $Label
 @onready var heartUIFull = $HeartUIFull
 @onready var heartUIEmpty = $HeartUIEmpty
+@onready var LOW_HEALTH = PlayerStats.max_health * 0.25
+@onready var HALF_HEALTH = PlayerStats.max_health * 0.5
+
+func _physics_process(delta):
+	update_healthbar()
+
+func update_healthbar():
+	var healthbar = $HealthBar
+	healthbar.max_value = PlayerStats.max_health
+	healthbar.value = PlayerStats.health
+	healthbar.visible = true
+	
+	if PlayerStats.health <= HALF_HEALTH and PlayerStats.health > LOW_HEALTH:
+		#Much hp has been lost turn yellow
+		healthbar.modulate = Color(1,1,0)
+	elif PlayerStats.health <= LOW_HEALTH:
+		#Health is critical, turn red
+		healthbar.modulate = Color(1, 0, 0)
 
 func set_hearts(value):
 	hearts = clampi(value, 0, max_hearts)
