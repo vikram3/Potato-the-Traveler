429a5cb585f73e944d1209ec9d5912f51d537408
Erebonia <52137104+Erebonia@users.noreply.github.com>
2024-04-10 18:57:30 -1000

    Boss Stats implemented / Customized HP Bar

--- Enemies/boss_golem.gd
+++ Enemies/boss_golem.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @onready var player = get_parent().find_child("Player")
 @onready var sprite = $Sprite2D
 @onready var progress_bar = $UI/BossHPBar
+@onready var animationPlayer = $AnimationPlayer
+@onready var hurtbox = $Hurtbox
  
 var direction : Vector2
 var DEF = 0
@@ -19,7 +21,6 @@ var health = 100:
 			find_child("FiniteStateMachine").change_state("ArmorBuff") 
  
 func _ready():
-	take_damage()
 	set_physics_process(false)
  
 func _process(_delta):
@@ -35,4 +36,20 @@ func _physics_process(delta):
 	move_and_collide(velocity * delta)
  
 func take_damage():
+	print("hp depleting")
 	health -= 10 - DEF
+	hurtbox.create_hit_effect()
+	hurtbox.start_invincibility(0.4)
+
+func _on_hurtbox_area_entered(area):
+	print("detected")
+	take_damage()
+	
+func _on_hurtbox_invincibility_started():
+	animationPlayer.play("Start")
+
+func _on_hurtbox_invincibility_ended():
+	animationPlayer.play("Stop")
+
+func _on_timer_timeout():
+	pass # Replace with function body.
