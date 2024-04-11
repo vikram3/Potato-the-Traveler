ed71cd18eb44639c0e8257a6895abaf1771848a3
Erebonia <52137104+Erebonia@users.noreply.github.com>
2024-04-10 21:30:54 -1000

    Added collisions and damage to lasers

--- Enemies/boss_golem.gd
+++ Enemies/boss_golem.gd
@@ -5,18 +5,21 @@ extends CharacterBody2D
 @onready var progress_bar = $UI/BossHPBar
 @onready var animationPlayer = $AnimationPlayer
 @onready var hurtbox = $Hurtbox
- 
+@export var KNOCKOUT_RANGE = 100
+@export var bossHP = 50
+
 var direction : Vector2
 var DEF = 0
  
-var health = 100:
+var health = bossHP:
 	set(value):
 		health = value
+		progress_bar.max_value = bossHP
 		progress_bar.value = value
 		if value <= 0:
 			progress_bar.visible = false
 			find_child("FiniteStateMachine").change_state("Death")
-		elif value <= progress_bar.max_value / 2 and DEF == 0:
+		elif value <= progress_bar.max_value / 2 and DEF == 0: # Phase two of the fight he gets tankier
 			DEF = 5
 			find_child("FiniteStateMachine").change_state("ArmorBuff") 
  
@@ -36,14 +39,19 @@ func _physics_process(delta):
 	velocity = direction.normalized() * 40
 	move_and_collide(velocity * delta)
  
-func take_damage():
-	health -= 30 - DEF
+func take_damage(area):
+	if area.damage - DEF > 0:
+		health -= area.damage - DEF
+	else:
+		health -= area.damage
 	hurtbox.create_hit_effect()
 	hurtbox.start_invincibility(0.4)
 
 func _on_hurtbox_area_entered(area):
+	velocity = area.knockback_vector * KNOCKOUT_RANGE
+	move_and_slide()
 	if health > 0:
-		take_damage()
+		take_damage(area)
 	
 func _on_hurtbox_invincibility_started():
 	animationPlayer.play("Start")
